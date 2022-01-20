3d084acfc5410ce099650672b57ccf5a5111dc8b
sarra-94 <gusmi.sarra@gmail.com>
2022-01-20 12:59:25 +0100

    update application live code session

--- application/src/Pages/Home/Home.js
+++ application/src/Pages/Home/Home.js
@@ -1,7 +1,17 @@
 import React from "react";
 
 const Home = () => {
-  return <div>Home Home</div>;
+  return (
+    <div>
+      {" "}
+      <div className="homeContent">
+        <h1>Welcome, this is the Home page</h1>
+        <h4>In this workshop we will : </h4>
+        <h4>- Create a complete project with component that consume api</h4>
+        <h4>- Use react router to route the app</h4>
+      </div>
+    </div>
+  );
 };
 
 export default Home;
